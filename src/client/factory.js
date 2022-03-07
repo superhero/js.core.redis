05b4bc7f6f7ea1b8021861bbd49497972f3e7b96
Erik Landvall <erik@landvall.se>
2022-03-07 15:06:40 +0100

    moved to using a factory pattern between the locator and service layer to  make it easier to reuse the factory logic from third party

---
+++ src/client/factory.js
@@ -0,0 +1,42 @@
+const
+  redis               = require('redis'),
+  RedisClient         = require('.'),
+  RedisClientGateway  = require('./gateway'),
+  RedisConnection     = require('./service/connection'),
+  RedisHash           = require('./service/hash'),
+  RedisKey            = require('./service/key'),
+  RedisList           = require('./service/list'),
+  RedisOrdered        = require('./service/ordered'),
+  RedisPubsub         = require('./service/pubsub'),
+  RedisStream         = require('./service/stream'),
+  RedisTransaction    = require('./service/transaction')
+
+/**
+ * @memberof Redis.Client
+ * @extends {superhero/core/locator/constituent}
+ */
+class RedisClientFactory
+{
+  /**
+   * @returns {RedisClient}
+   */
+  create(console, redisOptions)
+  {
+    const
+      client        = redis.createClient(redisOptions),
+      gateway       = new RedisClientGateway(client),
+      connection    = new RedisConnection(gateway),
+      hash          = new RedisHash(gateway),
+      key           = new RedisKey(gateway),
+      list          = new RedisList(gateway),
+      ordered       = new RedisOrdered(gateway),
+      pubsub        = new RedisPubsub(gateway),
+      stream        = new RedisStream(gateway, console),
+      factory       = this.create.bind(this, console, redisOptions),
+      transaction   = new RedisTransaction(gateway)
+
+    return new RedisClient(gateway, factory, connection, hash, key, list, ordered, pubsub, stream, transaction)
+  }
+}
+
+module.exports = RedisClientFactory
--- src/client/locator.js
+++ src/client/locator.js
@@ -1,16 +1,6 @@
-const
-  redis               = require('redis'),
-  RedisClient         = require('.'),
-  RedisClientGateway  = require('./gateway'),
-  RedisConnection     = require('./service/connection'),
-  RedisHash           = require('./service/hash'),
-  RedisKey            = require('./service/key'),
-  RedisList           = require('./service/list'),
-  RedisOrdered        = require('./service/ordered'),
-  RedisPubsub         = require('./service/pubsub'),
-  RedisStream         = require('./service/stream'),
-  RedisTransaction    = require('./service/transaction'),
-  LocatorConstituent  = require('superhero/core/locator/constituent')
+const 
+RedisClientFactory = require('./factory'),
+LocatorConstituent = require('superhero/core/locator/constituent')
 
 /**
  * @memberof Redis.Client
@@ -26,20 +16,11 @@ class RedisClientLocator extends LocatorConstituent
     const
       console       = this.locator.locate('core/console'),
       configuration = this.locator.locate('core/configuration'),
-      options       = configuration.find('client/redis/gateway'),
-      client        = redis.createClient(options),
-      gateway       = new RedisClientGateway(client),
-      connection    = new RedisConnection(gateway),
-      hash          = new RedisHash(gateway),
-      key           = new RedisKey(gateway),
-      list          = new RedisList(gateway),
-      ordered       = new RedisOrdered(gateway),
-      pubsub        = new RedisPubsub(gateway),
-      stream        = new RedisStream(gateway, console),
-      factory       = this.locate.bind(this),
-      transaction   = new RedisTransaction(gateway)
+      redisOptions  = configuration.find('client/redis/gateway'),
+      factory       = new RedisClientFactory(),
+      client        = factory.create(console, redisOptions)
 
-    return new RedisClient(gateway, factory, connection, hash, key, list, ordered, pubsub, stream, transaction)
+    return client
   }
 }
 
