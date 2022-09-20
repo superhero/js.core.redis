97143c92178e69fafcba799419e7d1339a188d04
Erik Landvall <erik@landvall.se>
2022-09-20 16:53:26 +0200

    improved error handling

--- src/client/factory.js
+++ src/client/factory.js
@@ -23,28 +23,38 @@ class RedisClientFactory
    */
   create(console, config)
   {
-    const
-      client        = redis.createClient(config.gateway),
-      gateway       = new RedisClientGateway(client),
-      connection    = new RedisConnection(gateway),
-      hash          = new RedisHash(gateway),
-      key           = new RedisKey(gateway),
-      list          = new RedisList(gateway),
-      ordered       = new RedisOrdered(gateway),
-      pubsub        = new RedisPubsub(gateway),
-      stream        = new RedisStream(gateway, console),
-      factory       = this.create.bind(this, console, config),
-      transaction   = new RedisTransaction(gateway)
-
-    client.on('error', (error) => 
+    try
     {
-      console.error('redis client error:', error)
-      console.error('redis client error discovered, terminating process...')
-      console.error('redis client config...', config)
-      process.nextTick(() => process.kill(process.pid, 'SIGKILL'))
-    })
-
-    return new RedisClient(config, console, gateway, factory, connection, hash, key, list, ordered, pubsub, stream, transaction)
+      const
+        client        = redis.createClient(config.gateway),
+        gateway       = new RedisClientGateway(client),
+        connection    = new RedisConnection(gateway),
+        hash          = new RedisHash(gateway),
+        key           = new RedisKey(gateway),
+        list          = new RedisList(gateway),
+        ordered       = new RedisOrdered(gateway),
+        pubsub        = new RedisPubsub(gateway),
+        stream        = new RedisStream(gateway, console),
+        factory       = this.create.bind(this, console, config),
+        transaction   = new RedisTransaction(gateway)
+  
+      client.on('error', (error) => 
+      {
+        console.error('redis client error:', error)
+        console.error('redis client error discovered, terminating process...')
+        console.error('redis client config...', config)
+        process.nextTick(() => process.kill(process.pid, 'SIGKILL'))
+      })
+  
+      return new RedisClient(config, console, gateway, factory, connection, hash, key, list, ordered, pubsub, stream, transaction)
+    }
+    catch(previousError)
+    {
+      const error = new Error('could not create the redis client')
+      error.chain = { previousError, config }
+      error.code = 'E_CORE_REDIS_CLIENT_FACTORY_CREATE'
+      throw error
+    }
   }
 }
 
